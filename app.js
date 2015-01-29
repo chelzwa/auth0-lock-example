4ce14af31f580926b8bdb27662677756a5d5578c
chelzwa <chelzwa@gmail.com>
2015-01-29 11:41:57 -0800

    Changed app.js to point at dist folder.

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var express = require("express");
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(__dirname + "/www"));
+app.use(express.static(__dirname + "/dist"));
 
 app.listen(port);
