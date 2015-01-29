e9aff802c1783435abb7c41f138ff042f07a33d0
chelzwa <chelzwa@gmail.com>
2015-01-29 11:27:25 -0800

    First commit.

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('githubExample', ['ui.router', 'auth0', 'angular-storage', 'angular-jwt'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
---
+++ src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('githubExample')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
